2d935cef3146646d69c188b7b44781561bc027cb
wavilap <wavilap0509@sowdy.com>
2019-08-25 13:57:59 -0500

    Updates

---
+++ precache-manifest.a201060bfef2f35ca2e92755b936ab10.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "209a17962549b865b68d63eb5d6c3fac",
+    "url": "/project-service/index.html"
+  },
+  {
+    "revision": "0cbd150a360cefac8a66",
+    "url": "/project-service/static/js/2.08a6870e.chunk.js"
+  },
+  {
+    "revision": "3f9b61c5403ae8ba2afa",
+    "url": "/project-service/static/js/main.f11cb131.chunk.js"
+  },
+  {
+    "revision": "50e11dbfe1d32511543a",
+    "url": "/project-service/static/js/runtime~main.aafe3e1a.js"
+  }
+]);
